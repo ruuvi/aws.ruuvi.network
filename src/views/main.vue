dc5cc68b49bec58df652900115785feb9282e191
Otso Jousimaa <otso@ojousima.net>
2020-03-18 20:52:15 +0200

    Plot all data

--- src/views/main.vue
+++ src/views/main.vue
@@ -10,6 +10,9 @@
 </template>
 
 <script>
+import { getParser } from 'ojousima.ruuvi_endpoints.ts'
+var ColorHash = require('color-hash');
+
 export default {
   name: 'Main',
   data:function(){
@@ -19,28 +22,81 @@ export default {
     }
   },
   methods: {
+    hexToUnsignedInt(inputStr) {
+      let hex  = inputStr.toString();
+      let Uint8Array = new Array();
+      for (let n = 0; n < hex.length; n += 2) {
+        Uint8Array.push(parseInt(hex.substr(n, 2), 16));
+      }
+      return Uint8Array;
+    },
+    parseRuuviPayload(hexString)
+    {
+      hexString = hexString.toLowerCase();
+      let payloadStart = hexString.search("ff9904") + 6;
+      if(6 > payloadStart){
+        return [0];
+      }
+      else {
+        return this.hexToUnsignedInt(hexString.substr(payloadStart));
+      }
+    },
     onTagData(items){
-      console.log("Received");
+      //console.log("Received");
+      // Sort by timestamp
       items.sort(function(a, b) {
         let keyA = a.timestamp;
         let keyB = b.timestamp;
-        // Compare the 2 dates
         if (keyA < keyB) return -1;
         if (keyA > keyB) return 1;
         return 0;
       });
-      let datapoints = items.map(item => ({
-        t: new Date(item.timestamp * 1000), 
-        y: item.rssi})
-      );
-      this.chartdata = {
-      datasets: [
+
+      // Parse data
+      let ruuviDatas = [];
+      for( let i in items)
+      {
+        let dataString = this.parseRuuviPayload(items[i].data);
+        let parser = getParser(dataString);
+        let data = parser(dataString);
+        data.timestamp = items[i].timestamp;
+        data.rssiDB = items[i].rssi;
+        // Delete properties not charted
+        for ( let property in data)
         {
-          label: 'rssi',
-          backgroundColor: '#f87979',
-          data: datapoints
+          if(0 == data[property]
+            || null == data[property]
+            || typeof data[property] === 'undefined')
+          {
+            delete data[property];
+          }
+            
+          delete data["parsedAt"]
         }
-      ]
+        // Add charted data to array
+        ruuviDatas.push(data);
+      }
+      // Format datasets for ChartJS
+      let dataSets = [];
+      let colorHash = new ColorHash();
+      for (let property in ruuviDatas[0])
+      {
+          if("timestamp" == property)
+          {
+            continue;
+          }
+          let dataSet = {};
+          dataSet.label = property;
+          dataSet.data = ruuviDatas.map(data => ({
+            y: data[property],
+            t: data["timestamp"] * 1000}));
+          dataSet.hidden = true;
+          dataSet.backgroundColor = colorHash.hex(property);
+          console.log(dataSet.backgroundColor);
+          dataSets.push(dataSet);
+      }
+      this.chartdata = {
+        datasets: dataSets
      };
 
       this.loaded = true;
